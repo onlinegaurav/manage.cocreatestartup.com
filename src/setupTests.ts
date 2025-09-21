5299e6e7e957e719ebd706644905b4fcc23b366a
Gaurav Gupta <gauravgupta13@hotmail.com>
2025-09-21 23:57:01 +0530

    fixed tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// src/setupTests.ts  (CRA already loads this)
+beforeAll(() => {
+    window.alert = jest.fn(); // mock alert
+});
